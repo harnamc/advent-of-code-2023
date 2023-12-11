4550b278682493879d4b3b59096aac583ac4e562
Harnam Chana <harnamsc@amazon.co.uk>
2023-12-11 00:09:18 +0000

    add part 2 for day 2

--- day2/index.part2.js
+++ day2/index.part2.js
@@ -17,36 +17,45 @@ readlineInterface.on("line", (line) => {
     const gameIdAndResults = line.split(":");
     const gameId = gameIdAndResults[0].split(" ")[1];
     const gameResults = gameIdAndResults[1].split(";");
-    let isImpossible = false;
+    const colourMap = {
+        red: 1,
+        green: 1,
+        blue: 1,
+    };
 
     gameResults.map((gameResult) => {
         const individualGameResults = gameResult.trim().split(",");
 
         individualGameResults.map((individualGameResult) => {
-            // @ts-ignore
-            const valueToTest = parseInt(individualGameResult.match(/[0-9]+/)[0]);
+            const valueToTest = parseInt(
+                // @ts-ignore
+                individualGameResult.match(/[0-9]+/)[0]
+            );
 
             if (
-                (individualGameResult.includes("red") &&
-                    valueToTest > bagQuantity.red) ||
-                (individualGameResult.includes("green") &&
-                    valueToTest > bagQuantity.green) ||
-                (individualGameResult.includes("blue") &&
-                    valueToTest > bagQuantity.blue)
+                individualGameResult.includes("red") &&
+                valueToTest > colourMap.red
             ) {
-                isImpossible = true;
-                return;
+                colourMap.red = valueToTest;
+            }
+
+            if (
+                individualGameResult.includes("green") &&
+                valueToTest > colourMap.green
+            ) {
+                colourMap.green = valueToTest;
+            }
+
+            if (
+                individualGameResult.includes("blue") &&
+                valueToTest > colourMap.blue
+            ) {
+                colourMap.blue = valueToTest;
             }
         });
-
-        if (isImpossible) {
-            return;
-        }
     });
 
-    if (!isImpossible) {
-        sum += parseInt(gameId);
-    }
+    sum += colourMap.red * colourMap.green * colourMap.blue;
 });
 
 readlineInterface.on("close", () => {
